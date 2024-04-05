cf67bb21875214ad033ea2e4ed6f52010622aef4
ChidhvilasTanay <73900317+ChidhvilasTanay@users.noreply.github.com>
2024-04-06 03:03:24 +0530

    added security packages

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ const authRouter = require('./routes/auth')
 const jobRouter = require('./routes/jobs')
 const authentication = require('./middleware/authentication')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const { RateLimiter } = require('rate-limiter');
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowsMs : 15 * 60 * 1000,
+  max: 100
+}))
 
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // routes
